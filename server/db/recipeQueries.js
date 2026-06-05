1d84e9057d207f294574ee36d1704f29d8e1e39e
Chinasa <kamogelomba@gmail.com>
2026-06-05 10:14:38 +0200

    worked on recipe queries

--- server/db/recipeQueries.js
+++ server/db/recipeQueries.js
@@ -2,8 +2,20 @@ import pool from "./connection";
 import pg from "pg"
 
 export const recipeQueries = {
-      getAllRecipes: async () =>{
-            const reesult = await pool.query('SELECT recipe_name,recipe_difficulty')
+      getAllRecipes: async (userId) =>{
+            const result = await pool.query('SELECT recipe_name,difficulty,recipe_id,preptime,cooktime,image_url FROM recipes WHERE user_id = $1',[userId]);
+            return result.rows;
+      },
+      getRecipeDetails: async (recipeID) =>{
+            const result = await pool.query('SELECT recipe_name,difficulty,recipe_id,preptime,cooktime,image_url FROM recipes WHERE recipe_id = $1', [recipeID]),
+            return result.rows
+      },
+      getInstructions: async (recipeID) =>{
+            const result = await pool.query('SELECT step_number,instruction FROM recipes WHERE recipe_id = $1', [recipeID]),
+            return result.rows
+      },
+      getIngredients: async (recipeID)=>{
+            const result = await pool.query('SELECT quantity FROM recipe_ingredients INNER JOIN ingredients ON recipe_ingredients_id =  ')
       }
 }
 
